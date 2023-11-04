c2ac17d95194eca04f4a761621390a869b9fdcc5
vinakhiem120 <89760611+vinakhiem120@users.noreply.github.com>
2023-11-04 09:56:24 +0700

    Create TwoStreamCNN.py
    modelCNN

---
+++ model/TwoStreamCNN.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt # for plotting
+import numpy as np # for transformation
+
+import torch # PyTorch package
+import torchvision # load datasets
+import torchvision.transforms as transforms # transform data
+import torch.nn as nn # basic building block for neural neteorks
+import torch.nn.functional as F # import convolution functions like Relu
+import torch.optim as optim # optimzer
+
+
+class TwoStreamCNN(nn.Module):
+    def __init__(self,
+                 streamA,
+                 streamB,
+                 ):
+        super().__init__()
