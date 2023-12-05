8dc5b8e5789293e1d0c66433aa5c1899b6dcddec
vinakhiem120 <89760611+vinakhiem120@users.noreply.github.com>
2023-12-05 11:09:29 +0700

    Update runner.py

--- runners/runner.py
+++ runners/runner.py
@@ -3,11 +3,9 @@ from model.TwoStreamCNN import TwoStreamCNN
 import torch
 from torch import nn
 import numpy as np
-from sklearn.model_selection import train_test_split
 from torch.optim import Adam
 from torchvision import datasets
 from torchvision import transforms
-from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
 from utils import utils
 device = utils.get_device()
@@ -32,7 +30,8 @@ class TwoStreamCNNrunner():
         self.transform = transform
         self.train_loader = None
         self.test_loader = None
-        self._init_data(0.2)
+        self.val_loader = None
+        self._init_data()
         
         # load checkpoint
         if 'ckpt_path' in config.path:
@@ -40,79 +39,100 @@ class TwoStreamCNNrunner():
                 last_state = torch.load(config.path.ckpt_path)
                 self.global_step = last_state['last_step']
                 self.model.load_state_dict(last_state['last_model'])
+    
+    def _init_data(self):
+        train_data = ASLDataset(data_path= (self.train_path), transform=self.transform)
+        test_data = ASLDataset(data_path= (self.test_path), transform=self.transform)
+        val_data = ASLDataset(data_path = self.val_loader,transform=self.transform)
+        self.train_loader = DataLoader(dataset = train_data,
+                                  batch_size= self.batch_size,
+                                  num_workers= self.num_workers
+                                  )
+        self.test_loader  = DataLoader(dataset=test_data,
+                                  batch_size= self.batch_size,
+                                  num_workers= self.num_workers)
+        self.val_loader = DataLoader(dataset=val_data,
+                                     batch_size = self.batch_size,
+                                     num_workers = self.num_workers)
         
         
+    def _get_optim(self,optim):
+        if str(optim) == "Adam":
+            return Adam(self._model.parameters(),lr=self.lr,)
+        return None
     def train(self):
         print('Start training on device {}'.format(device))
-        self._model = self._model.to(device)
+
+        self.model = self.model.to(device)
         best_val_loss = 1e9
 
         for epoch in range(self.epoch):
             print('Start epoch {}'.format(epoch))
 
-        # set up model state to training
-            self._model.train()
+            # set up model state to training
+            self.model.train()
 
-            for (xA, yA) in self.train_loader:
-                xA = xA.to(device=device)
-                xB,yB = next(iter(self.train_loader))
+            for (x,XN, y) in self.train_loader:
+                XN = XN.to(device=device)
+                x = x.to(device=device)
+                y = y.to(device=device)
 
-                xB = xB.to(device=device)
-                yA = yA.to(device=device)
-                y_pred = self._model(xA,xB)
+                y_pred = self.model(x,XN)
 
-                loss = self.criterion(y_pred, yA)
+                loss = self.criterion(y_pred, y)
                 loss.backward()  # calculate gradient
                 self.optimizer.step()  # update model parameters by gradient
                 self.optimizer.zero_grad()  # set gradient to zero for next loop
+                self.global_step += 1
 
+                # need to detach loss from calculating tree of pytorch
+                self.logger.add_scalar('train_loss', loss, global_step=self.global_step)
 
+            # set up model state to evaluating
+            self.model.eval()
 
-        # set up model state to evaluating
-        self.model.eval()
+            with torch.no_grad():
+                avg_loss = 0
+                avg_acc = 0
+                for (x,XN, y) in self.val_loader:
+                    XN = XN.to(device=device)
+                    x = x.to(device=device)
+                    y = y.to(device=device)
 
-        with torch.no_grad():
-            avg_loss = 0
-            avg_acc = 0
-            for (x, y) in self.val_loader:
-                x = x.to(device=device, dtype=torch.float32)
-                y = y.to(device=device, dtype=torch.float32)
+                    y_pred = self.model(x,XN)
+                    loss = self.criterion(y_pred, y)
+                    avg_loss += loss
 
-                y_pred = self._model(x,x.next())
-                loss = self.criterion(y_pred, y)
-                avg_loss += loss
+                    avg_acc += torch.sum(torch.argmax(y_pred, dim=1) == torch.argmax(y, dim=1)) / y.shape[0]
 
-                avg_acc += torch.sum(torch.argmax(y_pred, dim=1) == torch.argmax(y, dim=1)) / y.shape[0]
+                avg_loss /= len(self.val_loader)
+                avg_acc /= len(self.val_loader)
 
-            avg_loss /= len(self.test_loader)
-            avg_acc /= len(self.test_loader)
+                self.logger.add_scalar('val_loss', avg_loss, global_step=epoch)
+                self.logger.add_scalar('accuracy', avg_acc, global_step=epoch)
 
-            self.logger.add_scalar('val_loss', avg_loss, global_step=epoch)
-            self.logger.add_scalar('accuracy', avg_acc, global_step=epoch)
+                if best_val_loss > avg_loss:
+                    best_val_loss = avg_loss
+                    best_path = './ckpt/best_epoch.pth'
 
-            if best_val_loss > avg_loss:
-                best_val_loss = avg_loss
-                best_path = './ckpt/best_epoch.pth'
+                    cur_state = {
+                        'last_step': self.global_step,
+                        'last_model': self.model.state_dict()
+                    }
+                    torch.save(cur_state, best_path)
 
-                cur_state = {
-                    'last_step': self.global_step,
-                    'last_model': self.model.state_dict()
-                }
-                torch.save(cur_state, best_path)  
-    
-    
-    
     def test(self):
         print('Start testing on device {}'.format(device))
-        self._model.eval()
+        self.model.eval()
 
         pred_labels = []
 
         with torch.no_grad():
-            for x in self.test_loader:
-                x = x.to(device=device, dtype=torch.float32)
+            for x,XN,y in self.test_loader:
+                XN = XN.to(device=device)
+                x = x.to(device=device)
 
-                y_pred = self._model(x)
+                y_pred = self.model(x,XN)
                 pred_labels.append(torch.argmax(y_pred, dim=1).numpy())
 
         save_data = np.concatenate(pred_labels, axis=None)
@@ -122,23 +142,5 @@ class TwoStreamCNNrunner():
 
         submission = pd.concat([pd.Series(range(1, len(save_data) + 1), name="ImageId"), results], axis=1)
         submission.to_csv(os.path.join(self.config.path.save_path, 'result.csv'), index=False)
-    
-    
-    def _init_data(self,validation_split):
-        train_data = ASLDataset(data_path= (self.train_path), transform=self.transform)
-        test_data = ASLDataset(data_path= (self.test_path), transform=self.transform)
-        self.train_loader = DataLoader(dataset = train_data,
-                                  batch_size= self.batch_size,
-                                  num_workers= self.num_workers
-                                  )
-        self.test_loader  = DataLoader(dataset=test_data,
-                                  batch_size= self.batch_size,
-                                  num_workers= self.num_workers)
-        
-        
-    def _get_optim(self,optim):
-        if str(optim) == "Adam":
-            return Adam(self._model.parameters(),lr=self.lr,)
-        return None
         
         
